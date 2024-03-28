1f48de0809f25e281ece1b55b699ea021fb39b47
KandulaBhaskar5 <158037011+KandulaBhaskar5@users.noreply.github.com>
2024-03-28 22:55:19 +0530

    Add files via upload

---
+++ precache-manifest.6aa42305b56e7dc5dead082c70245c5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d707077ca3ebdc6badf888e7c96a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fead24e97e46c4e4021",
+    "url": "/static/css/main.1315ce76.chunk.css"
+  },
+  {
+    "revision": "a2cfb66d660b5f4abe0e",
+    "url": "/static/js/2.9b70f09e.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/static/js/2.9b70f09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fead24e97e46c4e4021",
+    "url": "/static/js/main.55dad258.chunk.js"
+  },
+  {
+    "revision": "df154b2e3ec386281a21",
+    "url": "/static/js/runtime-main.44fc426b.js"
+  }
+]);
